a3ed21082c1178071bf5a4fe828d1857efa0604d
Aakash Saini <aakashsaini769@gmail.com>
2021-10-20 18:01:06 -0700

    Deployment Issue Fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
 import NavBar from './components/navbar/Navbar'
-import About from './components/about/About'
-import Projects from './components/projects/Projects'
-import Skills from './components/skills/Skills'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
 import Footer from './components/footer/footer'
 
 
